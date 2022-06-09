62b4fe67ca87a5794069cf62779fe93fbaee4b98
Username <albavmolinanyc@gmail.com>
2022-06-09 16:38:57 -0400

    creates board function

--- scrabble/word_board/board.py
+++ scrabble/word_board/board.py
@@ -1,5 +1,27 @@
-#initialize empty list which will hold row, columns for the board
-board = [[]]
+def board():
+    width = 15
+    height = 15
+    board = []
+    row = []
+    square = "| |"
+    for i in range(width):
+        row.append(square)
+    for i in range(height):
+        board.append(row)
+    while True:
+        for i in range(len(board)):
+            print(board[i])
+        break
+
+board()
+
+
+"""
+15x15 grid for scrabble
+"""
+
+
+
 
 
 
